15fcca9d853a7e3bb168d2db355c75e33b906a9b
luiggix <luiggix@gmail.com>
2023-05-05 15:24:51 -0600

    macti 1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='macti',
     packages=find_packages(),#:include=['macti', 'macti.SistemasLineales', 'macti.MetodoEuler', 'macti.PyNoxtli']),
     include_package_data=True,
-    package_data={'':['data/SistemasLineales/*.npy', 'data/MetodoEuler/*.npy', 'data/Derivada/Introduccion/.__ans_*']},
+    package_data={'':['data/SistemasLineales/*.npy', 'data/MetodoEuler/*.npy', 'data/Derivada/Introduccion/.__ans_*', 'data/Plantillas/*.ipynb']},
     version='1.0.0',
     description='Macti 2.0',
     author='Luis M. de la Cruz',
