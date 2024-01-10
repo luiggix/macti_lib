64f3349022394eb0e671ef013b725d38ea8c5d39
luiggix <luiggix@gmail.com>
2024-01-10 19:42:34 +0000

    resources + clean

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='macti',
     packages=find_packages(),#:include=['macti', 'macti.SistemasLineales', 'macti.MetodoEuler', 'macti.PyNoxtli']),
     include_package_data=True,
-    package_data={'':['data/SistemasLineales/*.npy', 'data/MetodoEuler/*.npy', 'data/Derivada/Introduccion/.__ans_*', 'data/Plantillas/*.ipynb']},
+    package_data={'':['data/resources/*']},
     version='2.0',
     description='Macti Lib',
     author='Luis M. de la Cruz',
