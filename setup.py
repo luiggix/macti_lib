e5f146604d74663a27d1a24dad0cdfd09f7e3c71
“luiggix” <luiggix@gmail.com>
2024-01-22 00:57:17 +0000

    release candidate

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='macti',
     packages=find_packages(include=['macti', 'macti.fdm', 'macti.PyNoxtli']),
     include_package_data=True,
-    package_data={'':['data/resources/*']},
+    package_data={'':['data/resources/*', 'data/resources/.nbgex']},
     version='2.0',
     description='Macti Lib',
     author='Luis M. de la Cruz',
