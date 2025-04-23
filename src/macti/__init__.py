c9fca281aa13a3ee574a5f0d8b089efda457d9f8
luiggix <luiggix@gmail.com>
2025-04-23 11:17:49 -0600

    sin mf6

--- src/macti/__init__.py
+++ src/macti/__init__.py
@@ -1,6 +1,5 @@
 import macti.eval  # Importa el subpaquete eval
 import macti.math  # Importa el subpaquete math
 import macti.vis  # Importa el subpaquete vis
-import macti.mf6  # Importa el subpaquete mf6
 
-__all__ = ["eval", "math", "vis", "mf6"]
+__all__ = ["eval", "math", "vis"]
