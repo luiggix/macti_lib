c4c076431a476ea54d6e33e2d4614b071ca0fec0
luiggix <luiggix@gmail.com>
2025-05-07 21:07:35 -0600

    sol. del bug archivo .ans en modo hub

--- src/macti/eval/evaluation.py
+++ src/macti/eval/evaluation.py
@@ -256,7 +256,15 @@ class Quiz():
         if self.__server == 'local':
             self.__ans_path = self.__course_path + ".ans" + os.sep + self.__topic
         elif self.__server == 'hub':
+            # TODO: solve this hack ...
+            dummy_path, self.__course = os.path.split(cp)
+#            print(dummy_path, self.__course)
+            dummy_path, self.__course = os.path.split(dummy_path)
+#            print(dummy_path, self.__course)
+            self.__course += os.sep
             self.__ans_path = self.__server_path + self.__course + ".ans" + os.sep + self.__topic
+#            print(self.__course)
+#            print(self.__ans_path)
         
         self.__quiz_num = qnum # Número del quiz
 
@@ -834,7 +842,8 @@ if __name__ == '__main__':
     print("Quiz number:", file_answer.quiz_num)
 
     quiz = Quiz(file_answer.quiz_num, 'local')
-
+    quiz2 = Quiz(file_answer.quiz_num)
+    
     print('\nVerbosidad de la ayuda : {} \n'.format(quiz.verb))
     
     print('Opción')
