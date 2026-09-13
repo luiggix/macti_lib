ed618d68bc446c10f844a043eb1d2d17371a93eb
luiggix <luiggix@gmail.com>
2026-09-13 17:16:45 -0600

    configurado con UV, actualización de math

--- src/macti/math/matem.py
+++ src/macti/math/matem.py
@@ -3,20 +3,23 @@ import numpy as np
 def eigen_land(Mat):
     # Cálculo de eigenvectores
     w, v = np.linalg.eig(Mat)  # w: eigenvalues, v: eigenvectors
+    
+    return w, v
 
-    # Impresión de los eigenvalores y eigenvectores
-    print('eigenvalores = {}'.format(w))
-    print('eigenvectores:\n {} \n {}'.format(v[:,0], v[:,1]))
+def print_eigen(A, w, v):
+    # Cálculo de eigenvectores
+    w, v = np.linalg.eig(Mat)  # w: eigenvalues, v: eigenvectors
 
     # Cálculo del ángulo entre los vectores.
     e0 = v[:,0] / np.linalg.norm(v[:,0])
     e1 = v[:,1] / np.linalg.norm(v[:,1])
-    angulo = np.arccos(np.dot(e0, e1)) * 180 / np.pi
-    print('ángulo entre eigenvectores = {}'.format(angulo)) 
+    angle = np.arccos(np.dot(e0, e1)) * 180 / np.pi
     
-    return w, v
-
-def print_Aulu(A, w, v):
+    # Impresión de los eigenvalores y eigenvectores
+    print('eigenvalores = {}'.format(w))
+    print('eigenvectores:\n {} \n {}'.format(v[:,0], v[:,1]))
+    print('ángulo entre eigenvectores = {}'.format(angle)) 
+    
     for i, l in enumerate(w):
         print(chr(119860) + chr(119906) + ' = {}'.format(A @ v[:,i]))
         print(chr(120582) + chr(119906) + ' = {}'.format(l * v[:,i]))
