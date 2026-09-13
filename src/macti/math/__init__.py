fcb4a661d45c63d6666c80ca7aea9225547fa2d9
luiggix <luiggix@gmail.com>
2026-09-13 17:29:49 -0600

    change print_AuLu to print_eig

--- src/macti/math/__init__.py
+++ src/macti/math/__init__.py
@@ -1,5 +1,5 @@
 # Desde el .[nombre del archivo] importa [lista de funciones] 
-from .matem import eigen_land, print_Aulu
+from .matem import eigen_land, print_eigen
 
 # que nombres se exportan si se usa from macti.matem import * 
-__all__ = ["eigen_land", "print_Aulu"]
+__all__ = ["eigen_land", "print_eigen"]
